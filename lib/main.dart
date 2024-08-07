e9e2dc6ba6d8e321ba1be946517917a08129dda4
Mihail <mihhail.kalashnikov@lockerbot.io>
2024-08-07 17:05:32 +0700

    feat: add to codemagic env variables

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'di/injections.dart';
-import 'firebase_options.dart';
 
 
 
@@ -20,9 +19,7 @@ void main() async {
 _iosStart() async {
   debugPrint('_iosStart()');
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
    LocalNotification().initialize();
 
   configureDependencies();
@@ -38,9 +35,7 @@ _iosStart() async {
 _androidStart() async {
   debugPrint('_androidStart()');
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   final fcmToken = await FirebaseMessaging.instance.getToken();
   debugPrint(fcmToken);
   FirebaseMessaging messaging = FirebaseMessaging.instance;
